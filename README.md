7302f57fbea30367dd026fa7ecafd5d5e89925bc
Mario Cuba <mario@mariocuba.net>
2011-11-21 22:49:43 -0430

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+## ci-wp
+
+Small Codeigniter model that serves as an interface for Wordpress. It performs basic operations such as getting an users informations, getting the latest posts or retrieving comments or categories.
+
+## Requirements
+
+- [Codeigniter 2.1](http://codeigniter.com) (tested with the latest commit of the develop branch as of 21/11/2011=
+- [Wordpress 3.1](http://wordpress.org) - database only. You don't actually _need_ the application, but it will make a lot easier to post stuff into the database through it.
+
+It might be backwards compatible with previous versions of Wordpress and Codeigniter.
+
+## To-do's:
+
+1. Add full CRUD functionality
+2. Add getComments, getPage, and getTags
+
+## Instructions:
+
+Just copy the __wp.php__ file into application/models and load it into your controller:
+
+		$this->load->model('wp.php');
+
+You can also auto-load it editing the appropiate variable inside autoload.php.
+
+If you don't have the __WP\_DATABASE__ constant defined in your __config/constants.php__ file, then you should substitute the __$cdb__ variable with the name of your _database group_ contaning the connection info of your Wordpress installation.
---
+++ wp.php
@@ -0,0 +1,204 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * class Wp - Common interface with Wordpress.
+ *
+ * This class adds interaction to the website and a Wordpress database.
+ * Contains common operations such as fetch posts, comments, and the modification of such.
+ *.
+ * This library shouldn't have any dependencies other than a correctly formed Wordpress database - not even the actual Wordpress installation.
+ *
+ * @author Mario Cuba <mario@mariocuba.net>
+ * @since 28/09/2011
+ *
+ */
+ 
+class Wp extends CI_Model {
+	
+	private $cdb = WP_DATABASE; // current database
+	
+	function __construct() {
+		parent::__construct();
+		
+		$this->cdb = $this->load->database($this->cdb, TRUE);
+	}
+	
+	/*
+	 * method getLatestPosts($amount, [$fields]) - fetches the last $amount posts from Wordpress.
+	 *
+	 * Fetches the last $amount posts from Wordpress, with the $fields data.
+	 *
+	 * @param int[$amount] - the number of posts to fetch.
+	 * @param array[$fields] - the database fields to fetch. Must be named correctly after Wordpress-based databases.  
+	 *
+	 * @return object - a Codeigniter MySQL Object containing the $fields data. FALSE if there are no records.
+	 * @author Mario Cuba <mario@mariocuba.net>
+	 *
+	 * @since 23/09/2011
+	 *
+	 */
+	 
+	function getLatestPosts($amount, $fields = array('ID', 'post_title', 'post_date', 'guid')) {
+		$fields = implode(', ', $fields);
+		
+		$this->cdb
+		->select($fields)
+		->where('post_type', 'post')
+		->where('post_status', 'publish')
+		->order_by('post_date', 'desc');
+		
+		$sql = $this->cdb->get('posts', $amount);
+		
+		if ($sql->num_rows() > 0) {
+			return $sql;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/*
+ 	* method getPost($id, $fields) - fetches a single post from the database.
+ 	*
+ 	* This method gets $fields from the post $id from the database, and returns the whole row if the data was found - false otherwise.
+ 	*
+ 	* @param int[$id] - the post to fetch.
+ 	* @param array[$fields] - the database fields to fetch.
+ 	*
+ 	* @return object - a MySQL object with the $id post. 
+ 	* @author Mario Cuba <mario@mariocuba.net>
+ 	*
+ 	* @since  21/11/2011 
+ 	*
+ 	*/
+	
+	function getPost($id, $fields = array('ID', 'post_title', 'post_content', 'post_date', 'guid', 'post_author')) {
+		$this->cdb
+		->select($fields)
+		->where('ID', $id)
+		->where('post_type', 'post')
+		->where('post_status', 'publish')
+		->limit(1);
+		
+		$sql = $this->cdb->get('posts');
+		
+		if ($sql->num_rows() > 0) {
+			$sql = $sql->row();
+			
+			return $sql;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	
+	/*
+ 	* method getTotalComments($id) - calculates the amount of comments of a single post.
+ 	*
+ 	* @param int[$id] - the post used to calculate comments.
+ 	* @return int - the number of comments.
+ 	*
+ 	* @author Mario Cuba <mario@mariocuba.net>
+ 	* @since  21/11/2011
+ 	*
+ 	*/
+ 	
+	function getTotalComments($id) {
+		$this->cdb
+		->select('comment_ID')
+		->from('comments')
+		->where('comment_post_ID', $id);
+		
+		return $this->cdb->get()->num_rows();
+	}
+	
+	/*
+ 	* method getPostMeta($key, $post) - fetches the meta information of a post.
+ 	*
+ 	* This method will get the meta information string from a post, matching $key.
+ 	* The meta information of a post is a special value that must be set apart from the regular post information.
+ 	*
+ 	* @param string[$key] - the meta key that holds the info
+ 	* @param int[$post] - the post to fetch the meta info.
+ 	*
+ 	* @return object - a MySQL object with the meta post. 
+ 	* @author Mario Cuba <mario@mariocuba.net>
+ 	*
+ 	* @since  21/11/2011 
+ 	*
+ 	*/
+ 	
+	function getPostMeta($key, $post) {
+		$this->cdb
+		->select('meta_value')
+		->where('meta_key', $key)
+		->where('post_id', $post);
+		
+		$sql = $this->cdb->get('postmeta');
+		
+		if ($sql->num_rows() > 0) {
+			$sql = $sql->row();
+			return $sql->meta_value;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/*
+ 	* method getCategories($order) - gets the list of categories.
+ 	*
+ 	* @param string[$order] - the database column to order by. It takes a "table.column" string (usually form the terms table")
+ 	* @return object - a MySQL object with the list of categories.
+	*
+ 	* @author Mario Cuba <mario@mariocuba.net>
+ 	* @since  21/11/2011 
+ 	*
+ 	* @todo: Code an easier way to order the result set.
+ 	*
+ 	*/
+ 	
+	function getCategories($order = 'terms.term_id') {
+		$this->cdb
+		->select('name', 'slug')
+		->from('terms')
+		->join('term_taxonomy', 'terms.term_id = term_taxonomy.term_id')
+		->where('taxonomy', 'category')
+		->order_by($order);
+		
+		$sql = $this->cdb->get();
+		
+		if ($sql->num_rows() > 0) {
+			return $sql;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/*
+ 	* method getUserData($id, $fields) - fetches the user information from the database.
+ 	*
+ 	* @param int[$id] - the user id.
+ 	* @param array[$fields] - the database fields to fetch.
+ 	*
+ 	* @return object - a MySQL object with the user information. 
+ 	* @author Mario Cuba <mario@mariocuba.net>
+ 	*
+ 	* @since  21/11/2011 
+ 	*
+ 	*/
+ 	
+	function getUserData($id, $fields = array('user_login', 'user_nicename', 'user_email', 'display_name', 'user_url')) {
+		$this->cdb
+		->select($fields)
+		->where('ID', $id)
+		->limit(1);
+		
+		$sql = $this->cdb->get('users');
+		
+		if ($sql->num_rows() > 0) {
+			$sql = $sql->row();
+			return $sql;
+		} else {
+			return FALSE;
+		}
+	}
+}
